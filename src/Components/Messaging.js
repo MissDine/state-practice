912e249780763d1a97666789ea1b4195ed35e3ab
MissDine <enidnyatichi1212@gmail.com>
2022-05-24 20:41:58 +0300

    Fixed typo

--- src/Components/Messaging.js
+++ src/Components/Messaging.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
-class Messaging extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            message : "Good morning Enid"
-        }
+// State in Class component
+// class Messaging extends React.Component{
+//     constructor(){
+//         super()
+//         this.state = {
+//             message : "Good morning Enid"
+//         }
+//     }
+//     changeMessage (){
+//         this.setState({
+//             message : "Thank you for subscribing"
+//         })
+//     }
+//     render(){
+//         return(
+//             <>
+//                 <h1>{this.state.message}</h1>
+//                 <button onClick = {() => this.changeMessage()}>Subscribe</button>
+//             </>
+//         )
+//     }
+// }
+
+const Messaging = () => {
+    const [message, setMessage] = useState("Good morning Enid")
+    function changeMessage(){
+        setMessage("Thank you for subscribing")
     }
-    changeMessage (){
-        this.setState({
-            message : "Thank you for subscribing"
-        })
-    }
-    render(){
-        return(
-            <>
-                <h1>{this.state.message}</h1>
-                <button onClick = {() => this.changeMessage()}>Subscribe</button>
-            </>
-        )
-    }
+    return(<>
+    <h1>{message}</h1>
+        <button onClick = {() => changeMessage()}>Subscribe</button>
+    </>)
+        
 }
 export default Messaging
