fe654244a4f54c31254f294268a9c6ec56aea25f
MissDine <enidnyatichi1212@gmail.com>
2022-05-28 11:48:39 +0300

    Feat:Edit

--- src/Components/Messaging.js
+++ src/Components/Messaging.js
@@ -1,37 +1,37 @@
-import React, {useState} from "react";
+import React from "react";
 
 // State in Class component
-// class Messaging extends React.Component{
-//     constructor(){
-//         super()
-//         this.state = {
-//             message : "Good morning Enid"
-//         }
-//     }
-//     changeMessage (){
-//         this.setState({
-//             message : "Thank you for subscribing"
-//         })
-//     }
-//     render(){
-//         return(
-//             <>
-//                 <h1>{this.state.message}</h1>
-//                 <button onClick = {() => this.changeMessage()}>Subscribe</button>
-//             </>
-//         )
-//     }
-// }
-
-const Messaging = () => {
-    const [message, setMessage] = useState("Good morning Enid")
-    function changeMessage(){
-        setMessage("Thank you for subscribing")
+class Messaging extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            message : "Good morning Enid"
+        }
     }
-    return(<>
-    <h1>{message}</h1>
-        <button onClick = {() => changeMessage()}>Subscribe</button>
-    </>)
-        
+    changeMessage (){
+        this.setState({
+            message : "Thank you for subscribing"
+        })
+    }
+    render(){
+        return(
+            <>
+                <h1>{this.state.message}</h1>
+                <button onClick = {() => this.changeMessage()}>Subscribe</button>
+            </>
+        )
+    }
 }
+
+// const Messaging = () => {
+//     const [message, setMessage] = useState("Good morning Enid")
+//     function changeMessage(){
+//         setMessage("Thank you for subscribing")
+//     }
+//     return(<>
+//     <h1>{message}</h1>
+//         <button onClick = {() => changeMessage()}>Subscribe</button>
+//     </>)
+        
+// }
 export default Messaging
